1aec37bf4cc196be1932d5a477c5be03093c8318
Rajnish Khatri <rajnish.khatri@gmail.com>
2023-05-29 19:26:29 -0500

    working version

--- src/test/java/TestRunner/testRunner.java
+++ src/test/java/TestRunner/testRunner.java
@@ -2,19 +2,14 @@ package TestRunner;
 
 //import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-//import io.cucumber.testng.CucumberOptions;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.CucumberOptions;
 
 public class testRunner {
 	
 	//@RunWith(Cucumber.class) //Junit execution
 
 	@CucumberOptions(
-			plugin = {"pretty", "html:target/DSALgo.html",
-					"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-					"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
-					}, //reporting purpose
+			plugin = {"pretty", "html:target/DSALgo.html"}, //reporting purpose
 			//plugin = {"pretty", "html:target/SignIn1.html"}, //reporting purpose
 			//plugin = {"pretty", "html:target/Register1.html"}, //reporting purpose
 			monochrome=false,  //console output color
